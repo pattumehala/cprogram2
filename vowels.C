071136b7342b07a218788d2250053ea6d4044d8d
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-01-17 15:54:21 +0530

    vowels.C
    vowel or consonant

---
+++ vowels.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter the character");
+scanf("%c",&ch);
+if(ch=='a'||'e'||'i'||'o'||'u')
+{
+printf("The given character is a vowel");
+}
+else
+{
+printf("The given character is a consonant");
+}
+return 0;
+}
+
+
+
+
+
+
+
+
